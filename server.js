6f9d7950669c3849930f6a974ece664252dd5a28
Alex Johnson <alex@tablelist.com>
2015-04-18 17:56:25 -0400

    Attempting to POST to /language.php of social.bioware.com in order to scrape player stats

--- server.js
+++ server.js
@@ -4,39 +4,75 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
-	// Let's scrape Anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
+app.get('/:id', function(req, res){
+
+	var player = req.params.id;
+	var platform = "pc";
+	url = 'n7hq/home/challenges/?name=' + player + '%20&platform=' + platform;
+
+	if (!player) {
+		res.send(401, "Player ID Required");
+	}
+
+
+	request.post({
+		url:'http://social.bioware.com/language.php', 
+		form: {
+			return_url: url, //'/home.php',
+			lang_id: 1
+		}
+	}, function(err, httpResponse, body){
+		
+		console.log(httpResponse);
+
+		console.log("\n");
+
+		console.log(body);
+
+		request('http://social.bioware.com/' + url, function(error, response, html){
+
+			console.log(error);
+			console.log(response);
+			console.log(html);
+
+			res.send(200, html);
+		});
+
+	})
+
+	return;
 
 	request(url, function(error, response, html){
-		if(!error){
-			var $ = cheerio.load(html);
-
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
-
-			$('.header').filter(function(){
-		        var data = $(this);
-		        title = data.children().first().text();
-		        release = data.children().last().children().text();
-
-		        json.title = title;
-		        json.release = release;
-	        })
-
-	        $('.star-box-giga-star').filter(function(){
-	        	var data = $(this);
-	        	rating = data.text();
-
-	        	json.rating = rating;
-	        })
+		
+		if (error) {
+			res.send(200, {status : "Error"} );
+			return;
 		}
 
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        })
+		res.send(200, html);
+		
+		var $ = cheerio.load(html);
 
-        res.send('Check your console!')
+		var title, release, rating;
+		var json = { title : "", release : "", rating : ""};
+
+		$('.header').filter(function(){
+		    var data = $(this);
+		    title = data.children().first().text();
+		    release = data.children().last().children().text();
+
+		    json.title = title;
+		    json.release = release;
+	    })
+
+	    $('.star-box-giga-star').filter(function(){
+	    	var data = $(this);
+	    	rating = data.text();
+
+	    	json.rating = rating;
+	    })
+
+        // res.send(200, json)
 	})
 })
 
